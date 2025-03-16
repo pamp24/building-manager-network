6a142686f36f98a8a73043849a6c1ec1340434c7
pamp24 <pamporakis.fotis@gmail.com>
2025-03-17 00:25:35 +0200

    First Commit

---
+++ src/main/java/com/BuildingManager/building_manager_network/BuildingManagerNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.BuildingManager.building_manager_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingManagerNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingManagerNetworkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BuildingManager/building_manager_network/BuildingManagerNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BuildingManager.building_manager_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingManagerNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
